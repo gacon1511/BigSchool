857bb49243ba53a6dba1c4002d5e6ea88d00f035
gacon1511 <hoaiphuc15987@gmail.com>
2021-07-02 18:14:28 +0700

    Initial commit

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,50 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Create()
+        {
+            //get list category
+            BigSchoolContext context = new BigSchoolContext();
+            Course objCourse = new Course();
+            objCourse.ListCategory = context.Categories.ToList();
+            return View(objCourse);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Course objCourse)
+        {
+            
+            BigSchoolContext context = new BigSchoolContext();
+            //Không xét valid LecturedId vì bằng user đăng nhập
+            ModelState.Remove("LecturerId");
+            if(!ModelState.IsValid)
+            {
+                objCourse.ListCategory = context.Categories.ToList();
+                return View("Create", objCourse);
+            }
+            //Lấy login user ID
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            objCourse.LecturerId = user.Id;
+
+            //add vào CSDL
+            context.Courses.Add(objCourse);
+            context.SaveChanges();
+            
+            //Trở về Home, Action Index
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
